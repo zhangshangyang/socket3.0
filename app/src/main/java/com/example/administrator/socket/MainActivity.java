5991821f6e83c21d88147e9ac51104cb87e62958
zhangshangyang <37860081+zhangshangyang@users.noreply.github.com>
2018-03-28 18:01:12 +0800

    login have some problem

--- app/src/main/java/com/example/administrator/socket/MainActivity.java
+++ app/src/main/java/com/example/administrator/socket/MainActivity.java
@@ -1,14 +1,23 @@
 package com.example.administrator.socket;
 
 import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.app.FragmentManager;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Handler;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.java_websocket.client.WebSocketClient;
 import org.java_websocket.drafts.Draft_10;
@@ -16,6 +25,55 @@ import org.java_websocket.handshake.ServerHandshake;
 
 import java.net.URI;
 import java.net.URISyntaxException;
+
+@SuppressLint("ValidFragment")
+class ViewDialogFragment extends DialogFragment {
+
+    public interface Callback {
+        void onClick(String userName, String password);
+    }
+
+    private Callback callback;
+
+    public void show(FragmentManager fragmentManager) {
+        show(fragmentManager, "ViewDialogFragment");
+    }
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        @SuppressLint("InflateParams") final View view = inflater.inflate(R.layout.login, null);
+        builder.setView(view)
+                .setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        if (callback != null) {
+                            EditText et_userName = view.findViewById(R.id.username);
+                            EditText et_password = view.findViewById(R.id.password);
+                            callback.onClick(et_userName.getText().toString(), et_password.getText().toString());
+                        }
+                    }
+                });
+        return builder.create();
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof Callback) {
+            callback = (Callback) context;
+        } else {
+            throw new RuntimeException(context.toString() + " must implement Callback");
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        callback = null;
+    }
+}
 
 public class MainActivity extends AppCompatActivity {
     private WebSocketClient webSocketClient;
@@ -41,6 +99,7 @@ public class MainActivity extends AppCompatActivity {
         textView=findViewById(R.id.editText);
         Button button = findViewById(R.id.button);
         Button button2 = findViewById(R.id.button2);
+        Button button3= findViewById(R.id.button3);
         state=1;
        // init();
         button.setOnClickListener(new View.OnClickListener() {
@@ -62,7 +121,19 @@ public class MainActivity extends AppCompatActivity {
             }
         }
         });
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showViewDialogFragment(view);
+            }
+        });
     }
+
+    public void showViewDialogFragment(View view) {
+        ViewDialogFragment viewDialogFragment = new ViewDialogFragment();
+        viewDialogFragment.show(getFragmentManager());
+    }
+
     //网络读取数据
     private void init() {
         new Thread(new Runnable() {
@@ -109,4 +180,7 @@ public class MainActivity extends AppCompatActivity {
             webSocketClient.close();
         }
     }
+   /* public void onClick(String userName, String password) {
+        Toast.makeText(MainActivity.this, "用户名: " + userName + " 密码: " + password, Toast.LENGTH_SHORT).show();
+    }*/
 }
